31486bf5e5b0b49d241f29c6dc5b9802ca9eb586
Bhavik Mistry <bhavik.mistry@gmail.com>
2011-12-08 15:58:47 -0500

    View for requests modified. CSS for view changed

--- protected/views/request/_view.php
+++ protected/views/request/_view.php
@@ -1,27 +1,24 @@
 <div class="view">
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+    <div class="requestid">	
 	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
 	<br />
+    </div>
+    
 
-	<b><?php echo CHtml::encode($data->getAttributeLabel('raised_by')); ?>:</b>
-	<?php echo CHtml::encode($data->raised_by); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('request_type')); ?>:</b>
-	<?php echo CHtml::encode($data->request_type); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('assigned_to')); ?>:</b>
-	<?php echo CHtml::encode($data->assignedTo->username); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('query')); ?>:</b>
+     
+    <div class="dateraised">
+            <b><?php echo CHtml::encode($data->getAttributeLabel('date_raised')); ?>:</b>
+            <?php echo Yii::app()->dateFormatter->formatDateTime(CHtml::encode($data->date_raised), 'medium', false); ?>
+            <br />
+    </div>
+       <br/>
+    
+    <div class="querycontent">
 	<?php echo CHtml::encode($data->query); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('date_raised')); ?>:</b>
-	<?php echo Yii::app()->dateFormatter->formatDateTime(CHtml::encode($data->date_raised), 'medium', false); ?>
-	<br />
+    </div>
+	
+       <br />
+       <br />
 
 </div>
--- protected/views/request/index.php
+++ protected/views/request/index.php
@@ -14,4 +14,8 @@ $this->menu=array(
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
+        'sortableAttributes'=>array(
+            'date_raised'=>'Date Raised',
+            'id'=>'Request Number',
+        ),
 )); ?>
