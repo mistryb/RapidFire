2f3316d1abfc759e4eeae41c5e022f66fadd692b
Bhavik Mistry <bhavik.mistry@gmail.com>
2012-06-03 21:58:15 -0400

    git add assets/9ae46c7f/less/bootstrap-yii.less git add protected/controllers/RequestController.php git add protected/models/Request.php git add protected/models/User.php git add protected/runtime/application.log git add protected/runtime/gii-1.1.10/ModelCode.php git add protected/views/layouts/main.php git add protected/views/request/_view.php git add protected/views/request/index.php git add protected/views/userRequest/_view.php
    git add protected/controllers/UserRequestController.php
    git add protected/models/UserRequest.php
    git add protected/views/userRequest/index.php

---
+++ protected/views/userRequest/_view.php
@@ -0,0 +1,24 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('rfi_id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->rfi_id),array('view','id'=>$data->rfi_id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($data->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('originator')); ?>:</b>
+	<?php echo CHtml::encode($data->originator); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('body')); ?>:</b>
+	<?php echo CHtml::encode($data->body); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date_created')); ?>:</b>
+	<?php echo CHtml::encode($data->date_created); ?>
+	<br />
+
+
+</div>
